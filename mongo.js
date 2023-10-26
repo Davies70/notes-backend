4416c6667311f1cc09f488fd8384338230be5137
Ajayi Davies <ajayidavies7@gmail.com>
2023-10-26 16:19:55 +0200

    eslint added

--- mongo.js
+++ mongo.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://samwil500:${password}@cluster0.tzdzm89.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://samwil500:${password}@cluster0.tzdzm89.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 // Note.find({}).then((result) => {
 //   result.forEach((note) => {
@@ -29,9 +29,9 @@ const Note = mongoose.model('Note', noteSchema);
 const note = new Note({
   content: 'GET and POST are the most important methods of the HTTP protocol',
   important: false,
-});
+})
 
 note.save().then((result) => {
-  console.log('note saved!');
-  mongoose.connection.close();
-});
+  console.log('note saved!')
+  mongoose.connection.close()
+})
